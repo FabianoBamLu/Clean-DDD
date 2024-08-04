da5d81cf13b873c0d537feb13d6be9308fbe0702
FabianoBamLu <fabiano@groupbam.com>
2024-08-04 15:03:28 -0300

    domain: separate answer required props in a interface

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,18 +1,33 @@
+// -- INTERFACES
+
+interface AnswerProps
+{
+    authorId : string;
+    content : string;
+    questionId : string;
+}
+
+// -- TYPES
+
 export class Answer
 {
     // -- PROPERTIES
 
     public id ?: string;
     public content : string;
+    public authorId : string;
+    public questionId : string;
 
     // -- CONSTRUCTORS
 
     constructor(
-        content : string,
+        props : AnswerProps,
         id ?: string
         )
     {
-        this.content = content;
+        this.authorId = props.authorId;
+        this.content = props.content;
         this.id = id ?? crypto.randomUUID();
+        this.questionId = props.questionId;
     }
 }
